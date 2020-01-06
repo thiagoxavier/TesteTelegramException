eb32b61cdf77c30d53b860a9f67f84fe24676e73
thiagoxavier <xavier7132@gmail.com>
2020-01-06 13:29:24 -0300

    Add project files.

---
+++ Teste3/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Teste3.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        //Teste que gerará erro
+        [HttpGet]
+        public IActionResult Get(int n = 1)
+        {
+            var restu = n / 0;
+            return Ok();
+        }
+    }
+}
---
+++ TesteTelegramException/Controllers/WeatherForecastController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TesteTelegramException.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+        //endpoint com erro 
+        [HttpGet("Division/{number}")]
+        public IActionResult Division(int number)
+        {
+            var result = number / 0;
+            return Ok(result);
+        }
+
+    }
+}
